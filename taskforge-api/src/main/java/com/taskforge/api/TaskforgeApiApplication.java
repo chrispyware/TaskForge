88f876d9739039817f1dc0cf0e115f8dfd7cba69
chrispyware <thegreatchrispy@gmail.com>
2026-06-07 17:01:00 -0500

    Added api src structure

---
+++ taskforge-api/src/main/java/com/taskforge/api/TaskforgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.taskforge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskforgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskforgeApiApplication.class, args);
+	}
+
+}
